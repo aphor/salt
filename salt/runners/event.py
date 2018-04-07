4becf981d299d535033b5a78dd3e49aac03cf92a
Jeremy McMillan <jeremy.mcmillan@gmail.com>
2018-04-07 13:13:34 -0500

    add waitfor() to runners/event.py for #32956

--- salt/runners/event.py
+++ salt/runners/event.py
@@ -78,3 +78,118 @@ def send(tag, data=None):
     event = salt.utils.event.get_master_event(__opts__, __opts__['sock_dir'],
                                               listen=False)
     return event.fire_event(data, tag)
+
+def waitfor(tag, timeout=300):
+    '''
+    listen and wait (blocking) for an event with a given tag prefix,
+      for (Default timeout=300) seconds.
+
+    This is useful for asynchronous timing in orchestration jobs Where
+    prior orchestration states might have asynchronous effects,
+    or should wait for an event otherwise outside the orchestration job.
+
+    Hint: redeploy.sh might be a minion upgrade script.
+
+    :param tag: the tag prefix to match, satisfying the waitfor
+    :param timeout: seconds before waitfor will raise asyncio.TimeoutError
+
+    Orchestration example:
+    .. code-block:: yaml
+
+        # orch/rollout.sls
+
+        prepare for step 1:
+          salt.function:
+            - name: test.ping
+            - tgt: 'minion1'
+
+        step 1:
+          salt.function:
+            - name: cmd.shell
+            - tgt: 'minion1'
+            - require:
+              - prepare for step 1
+            - kwarg:
+                bg: False
+            - arg:
+              - |
+                redeploy.sh &&
+                salt-call event.send 'rolling/step/1/success'
+
+        step 1 success:
+          salt.runner:
+            - name: poc.waitfor
+            - arg:
+              - 'rolling/step/1/success'
+            - kwarg:
+                timeout: 600
+
+        prepare for step 2:
+          salt.function:
+            - name: test.ping
+            - tgt: 'minion2'
+            - require:
+              - step 1 success
+
+        step 2:
+          salt.function:
+            - name: cmd.shell
+            - tgt: 'minion2'
+            - require:
+              - prepare for step 2
+            - kwarg:
+                bg: False
+            - arg:
+              - |
+                redeploy.sh &&
+                salt-call event.send 'rolling/step/2/success'
+
+        step 2 success:
+          salt.runner:
+            - name: poc.waitfor
+            - parallel: True
+            - require:
+              - prepare for step 2
+            - arg:
+              - 'rolling/step/2/success'
+            - kwarg:
+                timeout: 600
+
+        rollout success:
+          salt.runner:
+            - name: event.send
+            - arg:
+              - 'rolling/complete'
+            - require:
+              - step 2 success
+
+
+    This is also useful to integrate other tools, allowing python or shell
+    scripts to block execution waiting for an arbitrary salt event.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt-run event.waitfor rolling/complete timeout=3600
+
+    '''
+    opts = __opts__
+
+    events = salt.utils.event.get_master_event(
+        opts,
+        opts['sock_dir'],
+        listen=True)
+
+    events.subscribe(tag=tag)
+    events.connect_pull()
+
+    try:
+        event_catch = events.get_event(wait=float(timeout), tag=tag)
+
+        if event_catch:
+            return True, event_catch
+
+    except asyncio.TimeoutError:
+        msg ='timeout waiting {d} seconds for event tag={}'.format(timeout, tag)
+        return False, msg
